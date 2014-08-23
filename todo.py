039138d8513811055051836c52bcb1e63774bda0
Doom Geek <geek@doomgeek.com>
2014-08-22 23:02:06 -0600

    Add some structure

--- todo.py
+++ todo.py
@@ -6,6 +6,73 @@ import sqlite3
 from datetime import date
 
 
+class SQLite3Database(object):
+    def __init__(self, path):
+        self._connection = sqlite3.connect(path)
+        self._managers = []
+
+    def register(self, manager):
+        manager.add_database(self)
+        self._managers.append(manager)
+
+    def create(self):
+        cursor = self._connection.cursor()
+        for manager in self._managers:
+            for query in manager.to_create():
+                cursor.execute(query)
+        cursor.close()
+
+    def get_connection(self):
+        return self._connection
+
+
+class TaskSQLManager(object):
+    def __init__(self, Task):
+        self._cls = Task.add_manager(self)
+
+    def add_database(self, database):
+        self._db = database
+
+    def to_create(self):
+        return ("create table task (id integer primary key autoincrement, "
+                "                   title text, due text)",)
+
+    def to_insert(self, task):
+        return ("insert into task (title, due) values (?, ?)",
+                (task.title, task.due))
+
+    def save(self, task):
+        connection = self._db.get_connection()
+        cursor = connection.cursor()
+        cursor.execute(*self.to_insert(task))
+        _id = cursor.lastrowid
+        cursor.close()
+        return _id
+
+
+class Task(object):
+    def __init__(self, title, due=None, pk=None):
+        self.title = title
+        self.due = due
+        self.pk = pk
+        for func in self._preprocessors:
+            func(self)
+
+    @classmethod
+    def add_preprocessors(cls, preprocessors):
+        cls._preprocessors = preprocessors
+        return cls
+
+    @classmethod
+    def add_manager(cls, manager):
+        cls._manager = manager
+        return cls
+
+    def save(self):
+        self.pk = self._manager.save(self)
+        return self
+
+
 def add_due_date_from_shortcut(task):
     due_date_shortcuts = {
         "today": date.today,
@@ -17,31 +84,17 @@ def add_due_date_from_shortcut(task):
         if sck in due_date_shortcuts:
             task.title = task.title.replace("^{}".format(shortcut),
                                             "").strip()
-            task.set_due(due_date_shortcuts[sck]())
+            task.due = due_date_shortcuts[sck]()
 
 
-preprocessors = [
-    add_due_date_from_shortcut,
-]
+preprocessors = (add_due_date_from_shortcut,)
 
+database = SQLite3Database(":memory:")
+database.register(TaskSQLManager(Task.add_preprocessors(preprocessors)))
+database.create()
 
-class Task(object):
-    def __init__(self, title, due=None):
-        self.set_title(title)
-        self.set_due(due)
-        for func in preprocessors:
-            func(self)
+task01 = Task("Get stuff done ^today #matt #is #awesome").save()
+task02 = Task("Eat Food ALLL DAY ^today #matt #is #awesome").save()
+task03 = Task("Ride my bike ALLL DAY ^today #matt #is #awesome").save()
 
-    def set_title(self, title):
-        self.title = title
-
-    def set_due(self, due):
-        self.due = due
-
-    def to_sql(self):
-        return ("insert into task (title, due) values (?, ?)", (self.title,
-                                                                self.due))
-
-
-task = Task("Get stuff done ^today #matt #is #awesome")
-print task.to_sql()
+import pdb; pdb.set_trace()
